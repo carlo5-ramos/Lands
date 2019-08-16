b7c51adbdefa738046c567e935d1e1f0433ca8ff
SoyBeckyOK <rebecasarai_gb@hotmail.com>
2019-08-16 10:38:59 -0600

    Video 11 finalizado

--- Lands/Lands/Lands/Services/ApiService.cs
+++ Lands/Lands/Lands/Services/ApiService.cs
@@ -99,6 +99,44 @@ namespace Lands.Services
             }
         }
 
+        public async Task<Response> GetList<T>(string urlBase, string servicePrefix, string controller)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = string.Format("{0}{1}", servicePrefix, controller);
+                var response = await client.GetAsync(url);
+                var result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = result,
+                    };
+                }
+
+                var list = JsonConvert.DeserializeObject<List<T>>(result);
+                return new Response
+                {
+                    IsSuccess = true,
+                    Message = "OK",
+                    Result = list,
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+            }
+        }
+
+
         public async Task<Response> GetList<T>(string urlBase, string servicePrefix, string controller, 
             string tokenType, string accessToken)
         {
@@ -282,6 +320,39 @@ namespace Lands.Services
                 {
                     IsSuccess = true,
                     Result = newRecord,
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+            }
+        }
+
+        public async Task<Response> Delete<T>(string urlBase, string servicePrefix, string controller, 
+            string tokenType, string accessToken, T model)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue(tokenType, accessToken);
+                var url = string.Format("{0}{1}/{2}", servicePrefix, controller, model.GetHashCode());
+                var response = await client.DeleteAsync(url);
+                var result = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    var error = JsonConvert.DeserializeObject<Response>(result);
+                    error.IsSuccess = false;
+                    return error;
+                }
+                return new Response
+                {
+                    IsSuccess = true,
                 };
             }
             catch (Exception ex)
