17f79cd8cbc92eb6723930e1ddf1ec25c77d71cc
SoyBeckyOK <rebecasarai_gb@hotmail.com>
2019-08-23 09:57:28 -0600

    video 18 finalizado

--- Lands/Lands/Lands/ViewModels/LandViewModel.cs
+++ Lands/Lands/Lands/ViewModels/LandViewModel.cs
@@ -11,7 +11,8 @@ namespace Lands.ViewModels
     {
         #region Attributes
         private ObservableCollection<Border> borders;
-
+        private ObservableCollection<Currency> currencies;
+        private ObservableCollection<Language> languages;
         #endregion
 
         #region Properties
@@ -22,6 +23,18 @@ namespace Lands.ViewModels
             get { return this.borders; }
             set { this.SetValue(ref this.borders, value); }
         }
+
+        public ObservableCollection<Currency> Currencies
+        {
+            get { return this.currencies; }
+            set { this.SetValue(ref this.currencies, value); }
+        }
+
+        public ObservableCollection<Language> Languages
+        {
+            get { return this.languages; }
+            set { this.SetValue(ref this.languages, value); }
+        }
         #endregion
 
         #region Constructors
@@ -29,6 +42,8 @@ namespace Lands.ViewModels
         {
             this.Land = land;
             this.LoadBorders();
+            this.Currencies = new ObservableCollection<Currency>(this.Land.Currencies);
+            this.Languages = new ObservableCollection<Language>(this.Land.Languages);
         }
 
         #endregion
