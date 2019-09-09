9197d396d67488511976b0800bb00774d5139a38
SoyBeckyOK <rebecasarai_gb@hotmail.com>
2019-09-09 09:48:59 -0600

    modificacion del proyecto api y confifuracio con el servidor y base de datos en azure

--- Lands/Lands.Backend/App_Start/IdentityConfig.cs
+++ Lands/Lands.Backend/App_Start/IdentityConfig.cs
@@ -53,7 +53,7 @@ namespace Lands.Backend
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 6,
+                RequiredLength = 5,
                 RequireNonLetterOrDigit = false,
                 RequireDigit = false,
                 RequireLowercase = false,
